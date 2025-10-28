5f53f1aa8f36458a3b1cbfc17057be98f9299f4a
Pradeep <deeppradeep1998@gmail.com>
2025-10-28 09:45:43 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+x="10"
+x+="5"
+res=int(x)//2
+print(res)
+
+#Output:52
+#Explanation:
+# Initially, x is a string "10".
+# After concatenation, x becomes "105".
+# Converting "105" to an integer gives 105.
+# Dividing 105 by 2 results in 52 (integer division).
+# The output of the code is 52.
